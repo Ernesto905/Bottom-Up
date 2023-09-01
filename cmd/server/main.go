5f6964e4c9ef7d9200835c8251a5603d1e65b861
ernesto905 <ernesto90643@gmail.com>
2023-09-01 09:45:02 -0400

    wringer http endpoint and hooked it up to the htmx frontend

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"text/template"
 )
 
+
+func homePage(w http.ResponseWriter, _ *http.Request) {
+  tmpl := template.Must(template.ParseFiles("../../public/index.html"))
+  tmpl.Execute(w, nil)
+}
+
+func wringer(w http.ResponseWriter, r *http.Request) {
+  branch := r.URL.Query().Get("branch") 
+  io.WriteString(w, fmt.Sprintf("You chose %s", branch))
+}
+
 func main() {
   fmt.Println("Listening on port 8080")
 
+  http.HandleFunc("/", homePage) 
 
-  h1 := func(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("../../public/index.html"))
-    tmpl.Execute(w, nil)
-  }
-
-  http.HandleFunc("/", h1) 
+  http.HandleFunc("/wringer", wringer) 
 
   // Creates the web server
   http.ListenAndServe(":8000", nil) 
