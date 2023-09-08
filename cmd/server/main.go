1e452c101f8358ddada53d7425c53f0bbb010965
ernesto905 <ernesto90643@gmail.com>
2023-09-08 09:38:26 -0400

    added openai functionality. User must input openai api key for the application to work successfully. Will consider, in the future, having a form on the site for users to input their api key directly

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+  "context"
+  "bytes"
 	"fmt"
-  "github.com/gorilla/mux"
-  "net/http"
+	"log"
+	"net/http"
+	"os"
 	"text/template"
+  openai "github.com/sashabaranov/go-openai"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 
@@ -22,7 +28,7 @@ func metaphysics(w http.ResponseWriter, r *http.Request) {
   }
 
   // Parse the template file
-  tmp, err := template.ParseFiles("../../public/wringer.html")
+  tmp, err := template.ParseFiles("../../public/wip.html")
   if err != nil {
     http.Error(w, "could not load template", http.StatusInternalServerError)
     return
@@ -45,7 +51,7 @@ func epistemology(w http.ResponseWriter, r *http.Request) {
   }
 
   // Parse the template file
-  tmp, err := template.ParseFiles("../../public/wringer.html")
+  tmp, err := template.ParseFiles("../../public/wip.html")
   if err != nil {
     http.Error(w, "could not load template", http.StatusInternalServerError)
     return
@@ -60,33 +66,77 @@ func epistemology(w http.ResponseWriter, r *http.Request) {
 }
 
 func ethics(w http.ResponseWriter, r *http.Request) {
+  response, err := initializeGPT("ethics")
+  if err != nil {
+    log.Printf("Error initializing GPT: %v", err)
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    return
+  }
+
   // Create a data structure to hold the template data
   data := struct {
     Branch string
+    GPT string
   }{
     Branch: "Ethics",
+    GPT: response, 
   }
 
   // Parse the template file
   tmp, err := template.ParseFiles("../../public/wringer.html")
   if err != nil {
-    http.Error(w, "could not load template", http.StatusInternalServerError)
+    log.Printf("Error parsing template: %v", err)
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
     return
   }
 
-  // Execute the template and write to ResponseWriter
-  err = tmp.Execute(w, data)
+  // Create a buffer to execute the template into
+  buffer := new(bytes.Buffer)
+  err = tmp.Execute(buffer, data)
   if err != nil {
-    http.Error(w, "could not execute template", http.StatusInternalServerError)
+    log.Printf("Error executing template: %v", err)
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
     return
   }
+
+  // Write the buffer content to the ResponseWriter
+  buffer.WriteTo(w)
 }
 
-func wringer(w http.ResponseWriter, r *http.Request) {
+func initializeGPT(branch string) (string, error) {
+  openAIKey := os.Getenv("OPENAI_API_KEY")
+  prompt := "Hello, you are an " + branch + " Philosopher" 
+  client := openai.NewClient(openAIKey)
+  resp, err := client.CreateChatCompletion(
+    context.Background(),
+    openai.ChatCompletionRequest{
+      Model: openai.GPT3Dot5Turbo,
+      Messages: []openai.ChatCompletionMessage{
+        {
+          Role:    openai.ChatMessageRoleUser,
+          Content: prompt,
+        },
+      },
+    },
+  )
+
+  if err != nil {
+    fmt.Printf("ChatCompletion error: %v\n", err)
+  }
+
+  // fmt.Println(resp.Choices[0].Message.Content)
+  return resp.Choices[0].Message.Content, nil
 }
 
 func main() {
+
   fmt.Println("Listening on port 8080")
+
+  err := godotenv.Load("../../.env")
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
 
   r := mux.NewRouter()
 
@@ -97,3 +147,5 @@ func main() {
 
   http.ListenAndServe(":8000", r)
 }
+
+
